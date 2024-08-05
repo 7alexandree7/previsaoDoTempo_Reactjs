b14317f52dfbcb25c39540b95f1aa3efe2c1a37c
Alexandre Oliveira <103005378+7alexandree7@users.noreply.github.com>
2024-08-05 12:51:01 -0300

    rquisição http

--- previsaoDoTempo_ReactJs/src/App.jsx
+++ previsaoDoTempo_ReactJs/src/App.jsx
@@ -2,22 +2,30 @@ import { useRef, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
 
 
 function App() {
 
   const inputRef = useRef()
+  const [weather, setWeather] = useState({})
 
 
-  function searchCity() {
-    console.log(inputRef.current.value)
+  async function searchCity() {
+    const city = inputRef.current.value
+    const key = '91f3807187ae857a89bc52667e1afd9a'
+    const idioma = 'pt_br'
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&lang=${idioma}&units=metric`
+
+    const apiInfo = await axios.get(url)
+    setWeather(apiInfo.data)
   }
 
 
   return (
 
     <div>
-      <h1>Previsão do tempo 91f3807187ae857a89bc52667e1afd9a</h1>
+      <h1>Previsão do tempo</h1>
       <input type="text" ref={inputRef} />
       <button onClick={searchCity}>Buscar</button>
     </div>
